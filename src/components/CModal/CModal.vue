73c71b0e2ab6b8f4008b6c1f92c06f7ac5142dbb
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2024-01-31 04:20:53 +0500

    refactor: added transition to Cmodal

--- src/components/CModal/CModal.vue
+++ src/components/CModal/CModal.vue
@@ -1,52 +1,55 @@
 <template>
-  <div
-    @click="closeModalOverlay"
-    class="flex items-center z-50 justify-center top-0 left-0 fixed w-screen h-screen bg-black bg-opacity-60"
-  >
+  <transition name="fade">
+    <!-- Ensure that modalValue is set before the component is mounted -->
     <div
-      @click.stop
-      class="modal-content bg-white w-[640px] p-7 max-h-[85vh] overflow-y-auto rounded-xl"
+      v-if="modalValue"
+      @click="closeModalOverlay"
+      class="flex items-center z-50 justify-center top-0 left-0 fixed w-screen h-screen bg-black bg-opacity-60"
     >
-      <div class="flex items-center justify-between">
-        <div class="font-bold text-2xl">
-          <slot name="title"></slot>
+      <div
+        @click.stop
+        class="modal-content bg-white w-[640px] p-7 max-h-[85vh] overflow-y-auto rounded-xl"
+      >
+        <div class="flex items-center justify-between">
+          <div class="font-bold text-2xl">
+            <slot name="title"></slot>
+          </div>
+
+          <div class="cursor-pointer" @click="closeModal">
+            <img src="/close.svg" alt="close icon" />
+          </div>
+        </div>
+        <hr class="my-7" />
+
+        <div class="">
+          <slot name="body"></slot>
         </div>
 
-        <div class="cursor-pointer" @click="closeModal">
-          <img src="/close.svg" alt="close icon" />
+        <hr class="my-7" />
+
+        <div class="flex items-center justify-end gap-4">
+          <slot name="footer"></slot>
         </div>
-      </div>
-      <hr class="my-7" />
-
-      <div class="">
-        <slot name="body"></slot>
-      </div>
-
-      <hr class="my-7" />
-
-      <div class="flex items-center justify-end gap-4">
-        <slot name="footer"></slot>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {
+  defineProps,
+  ref,
+} from 'vue';
 
 const props = defineProps(['modalValue', 'closeModal', 'closeModalOverlay'])
 </script>
 
 <style>
-.fade {
-  opacity: 0;
-  transition: opacity 0.3s;
-}
-
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s;
 }
+
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
