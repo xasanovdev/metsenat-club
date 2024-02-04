8b7219f17ee9fb972cb463b7fdd8c44fa082ca4d
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2024-02-04 17:06:12 +0500

    refactor: edit student modal

--- src/views/Dashboard/views/Students/components/EditStudentModal.vue
+++ src/views/Dashboard/views/Students/components/EditStudentModal.vue
@@ -1,41 +1,55 @@
 <template>
-  <CModal>
+  <CModal @childFunction="setChildFunction">
     <template #title>Tahrirlash</template>
     <template #body>
       <form class="max-w-[793px] w-full bg-white rounded-xl">
         <div class="grid grid-cols-1 gap-x-7 gap-y-[50px]">
           <div>
             <label>
-              {{ user?.full_name }}
+              {{ studentData?.full_name }}
               <span class="text-[12px] text-[#1D1D1F] mb-2 uppercase font-medium">
                 F.I.Sh. (Familiya Ism Sharif)
               </span>
-              <CInput v-model="user.full_name" placeholder="Abdullayev Abdulla Abdulla o’g’li" />
+              <CInput
+                v-model="studentData.full_name"
+                placeholder="Abdullayev Abdulla Abdulla o’g’li"
+              />
+              <span v-if="errors?.full_name" class="text-red-500">{{ errors?.full_name[0] }}</span>
             </label>
           </div>
           <div>
             <label>
-              {{ user?.phone }}
+              {{ studentData?.phone }}
               <span class="text-[12px] text-[#1D1D1F] mb-2 uppercase font-medium"
                 >Telefon raqam</span
               >
-              <CInput v-model="user.phone" placeholder="Abdullayev Abdulla Abdulla o’g’li" />
+              <CInput v-model="studentData.phone" placeholder="Abdullayev Abdulla Abdulla o’g’li" />
+              <span v-if="errors?.phone" class="text-red-500">{{ errors?.phone[0] }}</span>
             </label>
           </div>
           <div class="col-span-1">
             <label>
-              {{ user?.institute }}
+              {{ studentData?.institute }}
               <span class="text-[12px] text-[#1D1D1F] mb-2 uppercase font-medium">OTM</span>
-              <CDropdown v-model="user.institute.name" property="name" :options="instituteList" />
+              <CDropdown
+                v-model="studentData.institute.name"
+                property="name"
+                :options="instituteList"
+              />
+              <span v-if="errors?.institute" class="text-red-500">{{ errors?.institute[0] }}</span>
             </label>
           </div>
           <div>
             <label>
-              {{ user?.contract }}
+              {{ studentData?.contract }}
               <span class="text-[12px] text-[#1D1D1F] mb-2 uppercase font-medium"
                 >Kontrakt summa</span
               >
-              <CInput v-model="user.contract" placeholder="Abdullayev Abdulla Abdulla o’g’li" />
+              <CInput
+                v-model="studentData.contract"
+                placeholder="Abdullayev Abdulla Abdulla o’g’li"
+              />
+              <span v-if="errors?.contract" class="text-red-500">{{ errors?.contract[0] }}</span>
             </label>
           </div>
         </div>
@@ -64,13 +78,20 @@ import CInput from '@/components/CInput/CInput.vue';
 import CModal from '@/components/CModal/CModal.vue';
 import { useFetch } from '@/composables/useFetch';
 import router from '@/router';
-import { useDataStore } from '@/stores/data';
 
-const store = useDataStore()
+const studentData = ref('')
 
 const { put, get } = useFetch()
 
 const route = useRoute()
+
+const errors = ref(null)
+
+const childFunction = ref(null)
+
+const setChildFunction = (func) => {
+  childFunction.value = func
+}
 
 const instituteList = ref([])
 
@@ -83,40 +104,57 @@ const fetchInstituteList = async () => {
   }
 }
 
+const fetchData = async () => {
+  try {
+    console.log('asfsa')
+    const response = await get(`student-detail/${route.params.id}`)
+
+    studentData.value = response
+
+    console.log(studentData.value)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 onMounted(() => {
   fetchInstituteList()
-})
-
-const studentData = store?.studentsList?.results?.find((student) => student?.id == route.params.id)
-
-const user = ref({
-  id: '',
-  full_name: studentData?.full_name,
-  phone: studentData?.phone,
-  institute: studentData?.institute,
-  type: studentData?.type,
-  contract: studentData?.contract
+  fetchData()
 })
 
 const updateStudent = async () => {
   const selectedInstitute = computed(() => {
     return instituteList.value.find(
-      (item) => item.name.trim() === user.value.institute?.name.trim()
+      (item) => item.name.trim() === studentData.value.institute?.name.trim()
     )
   })
 
   try {
-    await put(`student-update/${studentData.id}/`, {
-      id: studentData.id,
+    const response = await put(`student-update/${studentData.value.id}/`, {
+      id: studentData.value.id,
       institute: selectedInstitute.value?.id,
-      full_name: user.value.full_name,
-      phone: user.value.phone,
-      type: user.value.type?.name === 'Bakalavr' ? 1 : 2,
-      contract: user.value.contract
+      full_name: studentData.value.full_name,
+      phone: studentData.value.phone,
+      type: studentData.value.type?.name === 'Bakalavr' ? 1 : 2,
+      contract: studentData.value.contract
     })
 
-    router.push({ name: 'Students' })
-    document.body.classList.remove('overflow-hidden')
+    if (
+      Array.isArray(response.full_name) ||
+      Array.isArray(response.contract) ||
+      Array.isArray(response.phone) ||
+      Array.isArray(response.institute)
+    ) {
+      errors.value = response
+    } else {
+      console.log(childFunction.value)
+      childFunction.value()
+
+      errors.value = null
+    }
+    console.log(response)
+    // router.push({ name: 'Students' })
+    // document.body.classList.remove('overflow-hidden')
   } catch (error) {
     console.error('Error fetching user:', error)
   }
