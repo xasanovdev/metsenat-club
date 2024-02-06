10515bc3f6aabb749ab07d8aed48c0d41fb9af3d
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2024-02-05 21:02:54 -0800

    refactor: deleted useless utils files

--- src/utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(timestamp) {
-  const dateObject = new Date(timestamp)
-
-  const day = dateObject.getDate().toString().padStart(2, '0')
-  const month = (dateObject.getMonth() + 1).toString().padStart(2, '0')
-  const year = dateObject.getFullYear()
-
-  return `${day}.${month}.${year}`
-}
--- src/utils/formatNumber.js
+++
@@ -1,9 +0,0 @@
-export function formatNumber(number) {
-  // Convert the number to a string and remove any non-digit characters
-  let cleanNumber = String(number).replace(/\D/g, '')
-
-  // Split the string into groups of three from the right
-  let formattedNumber = cleanNumber.replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
-
-  return formattedNumber
-}
